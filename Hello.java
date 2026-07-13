22d8d9c849a604aff2c02ae2a779f93f6cba89b9
monika rajpoot <rajpootmonika08@gmail.com>
2026-07-13 22:57:13 +0530

    Hello world

---
+++ Hello.java
@@ -0,0 +1,8 @@
+// first program of javaa
+// make sure that the class name and file name is same
+class Hello{
+    public static void main(String args[])
+     {
+          System.out.println("Hello, World!");
+     }
+}
