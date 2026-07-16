d2a5aca94c64b7900e2d426624a8f8915d882d5a
monika rajpoot <rajpootmonika08@gmail.com>
2026-07-16 11:46:28 +0530

    If Else

---
+++ IfElse.java
@@ -0,0 +1,23 @@
+public class IfElse {
+    public static void main(String args[])
+     { 
+        int marks=Integer.parseInt(args[0]);
+         if (marks >=90)
+         {System.out.println("1st position");
+
+         }
+        else{
+            System.out.println("2nd postion");
+        }
+     }
+    
+}
+
+/* int marks=45;
+         if (marks >=90)
+         {System.out.println("1st position");
+
+         }
+        else{
+            System.out.println("2nd postion");
+        } */
