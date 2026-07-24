926e9db3a4359b251707dd4af4c3c62071b880eb
monika rajpoot <rajpootmonika08@gmail.com>
2026-07-24 22:24:58 +0530

    Ternary Operator

---
+++ TernaryOp.java
@@ -0,0 +1,8 @@
+// Ternary Operator basic example
+public class TernaryOp {
+    public static void main (String[] args)
+    {   int number = 7;
+        System.out.println("The number is "+ ((number % 2 == 0) ? "Even" : "Odd") + ".");
+    }
+    
+}
