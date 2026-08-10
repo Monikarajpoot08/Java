f0cd824ee1cf88b57e85515c9001858f0cf2dbb1
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-10 18:11:40 +0530

    Left aligned right triangle

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+// Program to print a left aligned right triangle
+public class pattern1 {
+    public static void main (String[] args)
+    {
+        int n=5;
+        for (int i=0; i<n ; i++)
+        {
+            for (int j=0; j<i+1; j++)
+            {
+                 System.out.print("* ");
+            }
+             System.out.println();
+        }
+    }
+    
+}
