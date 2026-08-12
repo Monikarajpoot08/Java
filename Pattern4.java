1f8329b95e1a7c874bb16f9da51e972fc6566771
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-12 14:54:35 +0530

    right aligned right triangle

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+// Pattern to print a right aligned right triangle
+import java.util.Scanner;
+public class Pattern4 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); 
+      System.out.print("Enter number of rows ");
+      int n = sc.nextInt();
+       
+        for (int i=0; i<n ;i++)
+        {  // for loop to print the spaces
+            for (int j=0; j<n-i-1; j++)
+              {
+                 System.out.print(" ");
+              } 
+          // for loop to print the stars 
+           for(int j=0; j<=i ; j++)
+              {
+                 System.out.print("*");
+              }
+            System.out.println();
+        }
+    }
+    
+}
