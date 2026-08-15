6cbd1619d88532fdf00ee029f8a3999c08686f5e
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-15 16:23:42 +0530

    user defined exception

---
+++ Test.java
@@ -0,0 +1,14 @@
+//exception handling 
+//User defined exception
+class Test {
+    public static void main(String[] args) {
+        try{
+            int a=10/5;
+            System.out.println(a);
+        }
+        catch (ArithmeticException e)  {
+
+            System.out.println(" Division by 0 is not allowed");
+        }
+    }
+}
