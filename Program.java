e91c8ee06af246dbfa9a2ee9b8b0ef6f630b40d3
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-15 16:24:39 +0530

    practice program

---
+++ Program.java
@@ -0,0 +1,14 @@
+public class Program {
+    public static void main (String[] args)
+    {
+        for (int i=1; i<=10 ; i++)
+        {
+            for (int j=10; j<=20 ; j++)
+            {
+                  System.out.print(i*j + "\t");
+            }
+              System.out.println();
+        }
+    }
+    
+}
