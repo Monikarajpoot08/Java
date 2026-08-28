e7012325bde49d18ea084f1d4bf3b3dbdff1bf50
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-28 13:25:40 +0530

    array

---
+++ Array.java
@@ -0,0 +1,10 @@
+// Program to print an array
+public class Array {
+    public static void main(String[] args) {
+        
+        int [] arr = {2,3,22,11,43,54};
+        for (int i=0; i<6 ; i++){
+        System.out.println(arr[i]);
+    }
+}
+}
