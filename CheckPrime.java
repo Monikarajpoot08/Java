fb6bab536c575b4a73a5d9f00c78db1523cb2e7d
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-31 00:11:05 +0530

    Prime number

---
+++ CheckPrime.java
@@ -0,0 +1,29 @@
+// Program to check whether a number is prime or not
+import java.util.Scanner;
+
+public class CheckPrime {
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true;
+    }
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is NOT a Prime number.");
+        }
+
+        sc.close();
+    }
+} 
